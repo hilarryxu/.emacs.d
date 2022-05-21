15fa91c8b4898edd9f3486882f5cb91eaeb09aac
Larry Xu <hilarryxu@gmail.com>
2022-05-21 11:56:58 +0800

    chore: (gc-cons-percentage 0.6)

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -1,11 +1,11 @@
 ;; 不要自动备份，用 auto-save.el
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
 (require 'auto-save)
 (auto-save-enable)
-
 (setq auto-save-silent t)
-(setq auto-save-delete-trailing-whitespace t)
+; (setq auto-save-delete-trailing-whitespace t)
 
 (provide 'init-startup)
